ee04c1ba0b62169d700a0e3f8a490ddd7ef58960
Matthew Higgins <mthiggins1s@semo.edu>
2025-02-03 19:05:37 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+// Exercise 1:
+
+// Write a function applyDiscount that takes an array of prices and a callback function. 
+// The callback should apply a discount to each price. 
+// After the discount is applied, log the final prices.
+
+// function applyDiscount(prices, callback) {
+//     const discountedPrices = prices.map(price => callback(price));
+
+//     console.log(discountedPrices);
+// }
+
+// function discount(price) {
+//     const discountRate = 0.2;  // Applied a 20 percent discount
+//     return price - (price * discountRate);
+//   }
+
+//   const prices = [100, 200, 300, 400];
+//   applyDiscount(prices, discount); // logging the final prices with an applied discount
+
+//   // Output: [80, 160, 240, 360]
+
+
+
+
+// Exercise 5:
+
+// Write a function that uses setTimeout to log "Task completed!" after a 5-second delay.
+// Additionally, set up a setInterval to log "Still waiting..." every 2 seconds.
+
+// function logMessage() {
+//     console.log("Task Completed!")
+// }
+
+// setTimeout(logMessage, 5000);
+
+// function delayMessage() {
+//     console.log("Still waiting...")
+// }
+
+// setInterval(delayMessage, 2000);
